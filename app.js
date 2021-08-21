81e464905ef084e8fbdad29b2f94d5f7f08e9fea
Sean <robosean00@gmail.com>
2021-08-21 18:43:42 -0400

    like everything
    the entire rest of the basic bot, including watching for keywords and replying to them.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
+require('dotenv').config(); // imports
 const Discord = require('discord.js');
-const Intents = Discord.Intents;
+const replies = require('./replies.json');
+const Intents = Discord.Intents; // i dont want to type Discord constantly.
 
-const client = new Discord.Client({
+const client = new Discord.Client({ // declaring intents so i can actually do stuff
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
@@ -10,14 +11,31 @@ const client = new Discord.Client({
     ]
 });
 
+messageCheck = function(msg) {
+    let keywords = ["among us", "sus", "sussy", "impostor", "imposter",]
+    keywords.foreach((element, index) => {
+        if (msg.toLowerCase === element) {
+            return index+1
+        }
+    });
+
+    return 0
+}
+
 client.on('ready', () => {
     console.log(`${client.user.tag} is sussy and ready to intrude!`)
 
-    server = client.guilds.cache.get('757321307737030686');
+    let server = client.guilds.cache.get('757321307737030686');
 
-    channel = server.channels.cache.find(chan => chan.name === "general");
+    let channel = server.channels.cache.find(chan => chan.name === "general");
 
     channel.send('sussy!');
+});
+
+client.on('messageCreate', (message) => {
+    let value = messageCheck(message)
+
+    message.reply(replies.json[value]);
 });
 
 client.login(process.env.TOKEN);
